122dc3f6b2f487ab710049445e021cc37a19c234
Jeepien <github@Novosielski.com>
2023-04-13 15:48:13 -0400

    Update Synctime7300.py

--- Synctime7300.py
+++ Synctime7300.py
@@ -1,6 +1,24 @@
 #! /usr/bin/python3
 
 #Synctime7300
+
+#=======================================================================
+# Copyright 2022, 2023 Gary P. Novosielski
+#
+# This program is free software: you can redistribute it and/or
+# modify it under the terms of the GNU General Public License as
+# published by the Free Software Foundation, either version 3 of
+# the License, or (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program. If not, see <https://www.gnu.org/licenses/>.
+#=======================================================================
+
 # This script reads the system time and date for one minute in
 #  the future, which is the time we will set, then waits
 #  till HH:MM:00, and sends that time, date, and zone
